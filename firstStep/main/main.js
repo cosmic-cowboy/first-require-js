bd077e614d580d85e916849fe5d7fe324b9537ed
hiroo takaku <slgerkamp@gmail.com>
2014-11-30 17:02:54 +0900

    Mustacheの確認（入れ子）

--- firstStep/main/main.js
+++ firstStep/main/main.js
@@ -18,20 +18,31 @@ require([
   'scripts/content1',
   'scripts/content2',
   'scripts/first_mustache',
+  'scripts/second_mustache',
   'text!../assets/mustache/blankslate.mustache',
   'text!../assets/mustache/first_mustache.mustache',
+  'text!../assets/mustache/second_mustache.mustache',
+  'text!../assets/mustache/second_sub_mustache.mustache',
   'mustache',
   'jquery'
   ], function(
   content1,
   content2,
   first_mustacheJSON,
+  second_mustacheJSON,
   blankslateMustache,
   first_mustacheMustache,
+  second_mustacheMustache,
+  second_sub_mustacheMustache,
   Mustache,
   $){
     $('#result').append(Mustache.render(blankslateMustache, content1));
     $('#result').append(Mustache.render(blankslateMustache, content2));
     $('#result').append(Mustache.render(first_mustacheMustache, first_mustacheJSON));
+    $('#result').append(
+      Mustache.render(second_mustacheMustache, second_mustacheJSON, {
+        user: second_sub_mustacheMustache
+      })
+    );
   }
 );
